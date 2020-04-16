a5d647bbd53989a4c8a4c5c16ee5aa01194f4318
pillowpilot <carlos.federico005@gmail.com>
2020-04-16 17:12:44 -0400

    Removed unnecessary comments and better variable naming in SEIH. Removed unnecessary comments, extensive refactoring and added proper prediction error calculation in SolveODE. Proper optimization is now possible.

--- SEIH.py
+++ SEIH.py
@@ -14,9 +14,6 @@ def buildY_0(initial_conditions: InitialConditions, param: SEIHParameters):
     R_0: float = 0.0
     I_0: np.ndarray = np.zeros(3)
 
-    # self.param.ro = self.x[0]
-    # self.ro: float = self.param.ro
-
     I_0[0] = param.ro * Q_0  # infectados sintomas leves (iniciales) [I1]
     I_0[2] = param.ro * H_0  # infectados sintomas leves sin reportes (iniciales)  [I2]
     I_0[1] = ((I_0[0] + I_0[2]) * param.k) / (1 - param.k)  # # infectados sintomas severos (iniciales)  [I3]
@@ -32,94 +29,32 @@ def buildY_0(initial_conditions: InitialConditions, param: SEIHParameters):
 
 
 class SEIH:
-    def __init__(self, param: SEIHParameters, time_intervals: TimeIntervals,
-                 # initial_conditions: InitialConditions, real_data: RealData,
+    def __init__(self, parameters: SEIHParameters, time_intervals: TimeIntervals,
                  x: np.ndarray):
         #self.x: np.ndarray = x  # x are parameters to optimize with a metaheuristic approach ie genetic algorithms
         # looks like x has shape (1, 14)
-
         self.time_intervals: TimeIntervals = time_intervals
-
-        self.param: SEIHParameters = param
-
-        # H_0: float = initial_conditions.H
-        # D_0: float = initial_conditions.D
-        # Q_0: float = initial_conditions.Q
-        #
-        # Rh_0: float = 0.0
-        # R_0: float = 0.0
-        # I_0: np.ndarray = np.zeros(3)
-        #
-        # # self.param.ro = self.x[0]
-        # # self.ro: float = self.param.ro
-        #
-        # I_0[0] = param.ro * Q_0
-        # I_0[2] = param.ro * H_0
-        # I_0[1] = ((I_0[0] + I_0[2]) * param.k) / (1 - param.k)
-        #
-        # E_0: float = np.sum(I_0) * param.ro
-        #
-        # self.y_0 = np.zeros(10)
-        # self.y_0[0:2] = np.reshape([param.N - E_0 - np.sum(I_0), E_0], (2))
-        # self.y_0[2:5] = I_0
-        # self.y_0[5:10] = np.reshape([Q_0, Rh_0, H_0, R_0, D_0], (5))
-
-        # n_time_intervals: int = len(time_intervals)
-
-        # self.param.beta = self.x[1:n_time_intervals+1]
-        # self.param.drate = self.x[n_time_intervals + 1:n_time_intervals*2+1]
-
-        # self.lmbda: np.ndarray = np.array([1 - self.param.k - self.param.severe, self.param.k, self.param.severe])
-        # # TODO Maybe transpose lmbda (see https://stackoverflow.com/a/5954747/7555119)
-        # self.delta_l = self.param.delta[0]
-        # self.delta_r = self.param.delta[1]
-        # self.M: np.ndarray = np.zeros([10, 10])
-        # self.M[0, 2:5] = (-1) * self.param.beta[0] * self.param.contact
-        # self.M[1, 1] = (-1) * self.param.alpha
-        # self.M[1, 2:5] = self.param.beta[0] * self.param.contact
-        # self.M[2:5, 1:2] = np.transpose([self.lmbda*self.param.alpha])
-        # self.M[2:5, 2:5] = (-1) * np.diag(self.param.gamma)
-        # self.M[5:8, 2:5] = np.diag(self.param.gamma)
-        # self.M[5, 5] = (-1) * self.delta_l
-        # self.M[7, 7] = (-1) * self.delta_r
-        # self.M[8, 5] = self.delta_l
-        # self.M[8, 7] = (1 - self.param.drate[0]) * self.delta_r
-        # self.M[9, 7] = self.param.drate[0] * self.delta_r
-
+        self.parameters: SEIHParameters = parameters
         self.M: np.ndarray = np.zeros([10, 10])
-        self.initializeMMatrix(param)
+        self.initializeMMatrix(parameters)
 
-        # delay_infected: int = initial_conditions.t.toordinal() - real_data.infected['date'][0].toordinal()
-        # delay_dead: int = real_data.dead['date'][0].toordinal() - initial_conditions.t.toordinal()
-        # time: pd.Series = real_data.infected['date'][delay_infected:]
-        # tspan = (time[0].toordinal(), time[len(time) - 1].toordinal())
-        #
-        # sol = solve_ivp(self.evaluate, tspan, y_0)
-        #
-        # cumulative_infected = sol.y[5] + sol.y[7] + sol.y[8] + sol.y[9]
-        #
-        # plt.figure(1)
-        # plt.plot(sol.t, cumulative_infected, sol.t, sol.y[0])
-        # plt.yscale('log')
-        # plt.show()
-
-    def initializeMMatrix(self, param: SEIHParameters):
-        lmbda: np.ndarray = np.array([1 - param.k - param.severe, param.k, param.severe])
+    def initializeMMatrix(self, parameters: SEIHParameters):
+        lmbda: np.ndarray = np.array([1 - parameters.k - parameters.severe, parameters.k, parameters.severe])
         # TODO Maybe transpose lmbda (see https://stackoverflow.com/a/5954747/7555119)
-        delta_l = param.delta[0]
-        delta_r = param.delta[1]
+        delta_l = parameters.delta[0]
+        delta_r = parameters.delta[1]
 
-        self.M[0, 2:5] = (-1) * param.beta[0] * param.contact
-        self.M[1, 1] = (-1) * param.alpha
-        self.M[1, 2:5] = param.beta[0] * param.contact
-        self.M[2:5, 1:2] = np.transpose([lmbda * param.alpha])
-        self.M[2:5, 2:5] = (-1) * np.diag(param.gamma)
-        self.M[5:8, 2:5] = np.diag(param.gamma)
+        self.M[0, 2:5] = (-1) * parameters.beta[0] * parameters.contact
+        self.M[1, 1] = (-1) * parameters.alpha
+        self.M[1, 2:5] = parameters.beta[0] * parameters.contact
+        self.M[2:5, 1:2] = np.transpose([lmbda * parameters.alpha])
+        self.M[2:5, 2:5] = (-1) * np.diag(parameters.gamma)
+        self.M[5:8, 2:5] = np.diag(parameters.gamma)
         self.M[5, 5] = (-1) * delta_l
         self.M[7, 7] = (-1) * delta_r
         self.M[8, 5] = delta_l
-        self.M[8, 7] = (1 - param.drate[0]) * delta_r
-        self.M[9, 7] = param.drate[0] * delta_r
+        self.M[8, 7] = (1 - parameters.drate[0]) * delta_r
+        self.M[9, 7] = parameters.drate[0] * delta_r
 
     # See https://docs.scipy.org/doc/scipy/reference/generated/scipy.integrate.solve_ivp.html#scipy.integrate.solve_ivp
     # evaluate must be callable with the signature evaluate(t, y).
@@ -136,16 +71,16 @@ class SEIH:
 
         time_interval_index = max(0, np.count_nonzero(
             self.time_intervals.intervals.map(pd.Timestamp.toordinal) < time) - 1)
-        beta = self.param.beta[time_interval_index]
+        beta = self.parameters.beta[time_interval_index]
 
-        self.M[0, 2:5] = (-1) * beta * self.param.contact  # This is an analogous to the same block init at __init__
-        self.M[1, 2:5] = beta * self.param.contact  # This is an analogous to the same block init at __init__
-        self.M[0:2, 2:5] = self.M[0:2, 2:5] * (S / self.param.N)
+        self.M[0, 2:5] = (-1) * beta * self.parameters.contact  # This is an analogous to the same block init at __init__
+        self.M[1, 2:5] = beta * self.parameters.contact  # This is an analogous to the same block init at __init__
+        self.M[0:2, 2:5] = self.M[0:2, 2:5] * (S / self.parameters.N)
 
-        H_interval_index = max(0, np.count_nonzero(self.param.Hlimit < H) - 1)
-        drate = self.param.drate[H_interval_index]
+        H_interval_index = max(0, np.count_nonzero(self.parameters.Hlimit < H) - 1)
+        drate = self.parameters.drate[H_interval_index]
 
-        self.M[8:10, 7:8] = np.array([[1 - drate], [drate]]) * self.param.delta[1]
+        self.M[8:10, 7:8] = np.array([[1 - drate], [drate]]) * self.parameters.delta[1]
 
         rhs = self.M.dot(y)
 
--- SolveODE.py
+++ SolveODE.py
@@ -12,72 +12,83 @@ from SEIH import SEIH, buildY_0
 def ode(x: np.ndarray, *args):
     time_intervals = TimeIntervals(['01/22/2020'])
 
-    param = SEIHParameters()
-    param.contact = np.array([1.0, 1.0, 0.3])
-    param.severe = 0.2  #
-    param.alpha = 1.0 / 5.0
-    param.gamma = np.array([1.0 / 5.0, 1.0 / 14.0, 1.0 / 6.0])
-    param.delta = np.array([1.0 / 9.0, 1.0 / 15.0])
-    param.N = 100000000
-    param.In = 0  # casos importados
-    param.Hlimit = np.array([0, 1000000])  # limite de hospitalizaciones
-    # param.ro = 3.0
-    param.k = 0.3  # porcentaje de los que no se reportan
-    # param.beta = np.array([0.6])
-    # param.drate = np.array([0.5])
+    parameters = SEIHParameters()
+    parameters.contact = np.array([1.0, 1.0, 0.3])
+    parameters.severe = 0.2  #
+    parameters.alpha = 1.0 / 5.0
+    parameters.gamma = np.array([1.0 / 5.0, 1.0 / 14.0, 1.0 / 6.0])
+    parameters.delta = np.array([1.0 / 9.0, 1.0 / 15.0])
+    parameters.N = 100000000
+    parameters.In = 0  # casos importados
+    parameters.Hlimit = np.array([0, 1000000])  # limite de hospitalizaciones
+    parameters.k = 0.3  # porcentaje de los que no se reportan
+    parameters.beta = x[1:len(time_intervals) + 1]  # entre 0 y (2 o 3)
+    parameters.drate = x[len(time_intervals) + 1:]  # entre 0 y 1 (dead rate)
+    parameters.ro = x[0]  # entre 0 y (10 o 20)
 
-    # This gets overwritten!
-    param.beta = x[1:len(time_intervals) + 1]  # entre 0 y (2 o 3)
-    param.drate = x[len(time_intervals) + 1:]  # entre 0 y 1 (dead rate)
-
-    # print(x)
-    # print(x[0])
-    # print(x[1])
-    param.ro = x[0]  # entre 0 y (10 o 20)
-    # param.k = x[1]
-
-    initial_conditions = InitialConditions()
-    initial_conditions.t = pd.to_datetime('01/22/2020')
-    initial_conditions.S = param.N  # suceptible (puden infectarse)
-    initial_conditions.E = 0  # expuestos
-    initial_conditions.I1 = 0  # infectados sintomas leves
-    initial_conditions.I2 = 0  # infectados sintomas leves sin reportes
-    initial_conditions.I3 = 0  # infectados sintomas severos
-    initial_conditions.Q = 6  # en sus casas (I1, reportan -> Q)
-    initial_conditions.H = 1  # internados (I3, reportan -> H) [para saber la capacidad del sistema sanitario]
-    initial_conditions.D = 0  # dead
-    initial_conditions.R = 0  # recuperados
-    initial_conditions.Rh = 0  # (I2, despues cuarentena -> Rh)
+    ode_modeling_variables = InitialConditions()
+    ode_modeling_variables.t = pd.to_datetime('01/22/2020')
+    ode_modeling_variables.S = parameters.N  # suceptible (puden infectarse)
+    ode_modeling_variables.E = 0  # expuestos
+    ode_modeling_variables.I1 = 0  # infectados sintomas leves
+    ode_modeling_variables.I2 = 0  # infectados sintomas leves sin reportes
+    ode_modeling_variables.I3 = 0  # infectados sintomas severos
+    ode_modeling_variables.Q = 6  # en sus casas (I1, reportan -> Q)
+    ode_modeling_variables.H = 1  # internados (I3, reportan -> H) [para saber la capacidad del sistema sanitario]
+    ode_modeling_variables.D = 0  # dead
+    ode_modeling_variables.R = 0  # recuperados
+    ode_modeling_variables.Rh = 0  # (I2, despues cuarentena -> Rh)
 
     infected_data = read_data_file('testing_resources/cum_world.dat')
     dead_data = read_data_file('testing_resources/dcm_world.dat')
     recovered_data = read_data_file('testing_resources/rcm_world.dat')
     real_data = RealData(infected_data, dead_data, recovered_data)
 
-    y_0 = buildY_0(initial_conditions, param)
+    y_0 = buildY_0(ode_modeling_variables, parameters)
 
-    seih = SEIH(param, time_intervals, x)
+    seih = SEIH(parameters, time_intervals, x)
 
-    delay_infected: int = initial_conditions.t.toordinal() - real_data.infected['date'][0].toordinal()
-    delay_dead: int = real_data.dead['date'][0].toordinal() - initial_conditions.t.toordinal()
-    time: pd.Series = real_data.infected['date'][delay_infected:]
-    # convertir cada entry de time con toordinal para calcular los valores SOLO en esos puntos!
+    infected_offset: int = ode_modeling_variables.t.toordinal() - real_data.infected['date'][0].toordinal()
+    dead_offset: int = real_data.dead['date'][0].toordinal() - ode_modeling_variables.t.toordinal()
+    time: pd.Series = real_data.infected['date'][infected_offset:]
     time_span = (time[0].toordinal(), time[len(time) - 1].toordinal())
 
-    solution = solve_ivp(seih.evaluate, time_span, y_0)  # Ver doc sobre punto anterior.
+    solution = solve_ivp(seih.evaluate, time_span, y_0, t_eval=np.arange(time_span[0], time_span[1] + 1))
+    in_quarantine = solution.y[5]
+    hospitalized = solution.y[7]
+    recovered = solution.y[8]
+    dead = solution.y[9]
 
-    cumulative_infected = solution.y[5] + solution.y[7] + solution.y[8] + solution.y[9]  # Q, H, R, D
+    # TODO cumI = sum(y(:,[6 8:10]),2); Q+H+R+D? infected_theoretical es un BUEN NOMBRE?
+    infected_theoretical: np.ndarray = in_quarantine + hospitalized + recovered + dead  # Q, H, R, D
+    transformed_infected_theoretical: np.ndarray = np.log(infected_theoretical)
+    infected_real_data: np.ndarray = real_data.infected['infected'][infected_offset:].values  # pd.Series -> np.ndarray
+    transformed_infected_real_data: np.ndarray = np.log(infected_real_data)
+    infected_prediction_error = np.linalg.norm(transformed_infected_real_data - transformed_infected_theoretical)
 
-    # log y dist euclidanea
+    dead_theoretical: np.ndarray = dead[dead_offset:]
+    transformed_dead_theoretical: np.ndarray = np.log(dead_theoretical)
+    dead_real_data: np.ndarray = real_data.dead['dead']
+    transformed_dead_read_data: np.ndarray = np.log(dead_real_data)
+    dead_prediction_error = np.linalg.norm(transformed_dead_read_data - transformed_dead_theoretical)
 
+    prediction_error = infected_prediction_error + dead_prediction_error
 
-    return solution
+    # plt.figure(44)
+    # plt.plot(transformed_dead_theoretical)
+    # plt.plot(transformed_dead_read_data)
+    # plt.figure(55)
+    # plt.plot(transformed_infected_theoretical)
+    # plt.plot(transformed_infected_real_data)
+    # plt.show()
+
+    return prediction_error
 
 
 def main():
-    # x = np.array([3.00, 0.60, 0.30, 0.20, 0.95,
-    #               0.44, 0.51, 0.42, 0.02, 0.13,
-    #               0.48, 0.22, 0.33, 0.80])  # Optimized values from MATLAB code
+    x = np.array([3.00, 0.60, 0.30, 0.20, 0.95,
+                  0.44, 0.51, 0.42, 0.02, 0.13,
+                  0.48, 0.22, 0.33, 0.80])  # Optimized values from MATLAB code
 
     number_of_variables = 4
     bounds = [(2, 4)] * number_of_variables
